7b19bf7fc32b477136e3b166ac61775a7d92f368
由风 <youfeng11@outlook.com>
2025-12-22 20:28:53 +0800

    chore: 解决弃用与更新

--- kotlinx-serialization-json5/build.gradle.kts
+++ kotlinx-serialization-json5/build.gradle.kts
@@ -9,7 +9,7 @@ kotlin {
     // JVM 平台
     jvm {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
@@ -29,7 +29,7 @@ kotlin {
     }
 
     // WebAssembly
-    @OptIn(org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl::class)
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)  // 新注解
     wasmJs {
         browser()
         nodejs()
@@ -96,127 +96,6 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
-
-        // ===== Native 共享代码 =====
-        val nativeMain by creating {
-            dependsOn(commonMain.get())
-        }
-
-        val nativeTest by creating {
-            dependsOn(commonTest.get())
-        }
-
-        // Apple 平台
-        val appleMain by creating {
-            dependsOn(nativeMain)
-        }
-
-        val appleTest by creating {
-            dependsOn(nativeTest)
-        }
-
-        // iOS
-        val iosMain by creating {
-            dependsOn(appleMain)
-        }
-        iosX64().compilations["main"].defaultSourceSet.dependsOn(iosMain)
-        iosArm64().compilations["main"].defaultSourceSet.dependsOn(iosMain)
-        iosSimulatorArm64().compilations["main"].defaultSourceSet.dependsOn(iosMain)
-
-        val iosTest by creating {
-            dependsOn(appleTest)
-        }
-        iosX64().compilations["test"].defaultSourceSet.dependsOn(iosTest)
-        iosArm64().compilations["test"].defaultSourceSet.dependsOn(iosTest)
-        iosSimulatorArm64().compilations["test"].defaultSourceSet.dependsOn(iosTest)
-
-        // macOS
-        val macosMain by creating {
-            dependsOn(appleMain)
-        }
-        macosX64().compilations["main"].defaultSourceSet.dependsOn(macosMain)
-        macosArm64().compilations["main"].defaultSourceSet.dependsOn(macosMain)
-
-        val macosTest by creating {
-            dependsOn(appleTest)
-        }
-        macosX64().compilations["test"].defaultSourceSet.dependsOn(macosTest)
-        macosArm64().compilations["test"].defaultSourceSet.dependsOn(macosTest)
-
-        // watchOS
-        val watchosMain by creating {
-            dependsOn(appleMain)
-        }
-        watchosArm32().compilations["main"].defaultSourceSet.dependsOn(watchosMain)
-        watchosArm64().compilations["main"].defaultSourceSet.dependsOn(watchosMain)
-        watchosX64().compilations["main"].defaultSourceSet.dependsOn(watchosMain)
-        watchosSimulatorArm64().compilations["main"].defaultSourceSet.dependsOn(watchosMain)
-        watchosDeviceArm64().compilations["main"].defaultSourceSet.dependsOn(watchosMain)
-
-        val watchosTest by creating {
-            dependsOn(appleTest)
-        }
-        watchosArm32().compilations["test"].defaultSourceSet.dependsOn(watchosTest)
-        watchosArm64().compilations["test"].defaultSourceSet.dependsOn(watchosTest)
-        watchosX64().compilations["test"].defaultSourceSet.dependsOn(watchosTest)
-        watchosSimulatorArm64().compilations["test"].defaultSourceSet.dependsOn(watchosTest)
-        watchosDeviceArm64().compilations["test"].defaultSourceSet.dependsOn(watchosTest)
-
-        // tvOS
-        val tvosMain by creating {
-            dependsOn(appleMain)
-        }
-        tvosArm64().compilations["main"].defaultSourceSet.dependsOn(tvosMain)
-        tvosX64().compilations["main"].defaultSourceSet.dependsOn(tvosMain)
-        tvosSimulatorArm64().compilations["main"].defaultSourceSet.dependsOn(tvosMain)
-
-        val tvosTest by creating {
-            dependsOn(appleTest)
-        }
-        tvosArm64().compilations["test"].defaultSourceSet.dependsOn(tvosTest)
-        tvosX64().compilations["test"].defaultSourceSet.dependsOn(tvosTest)
-        tvosSimulatorArm64().compilations["test"].defaultSourceSet.dependsOn(tvosTest)
-
-        // Linux
-        val linuxMain by creating {
-            dependsOn(nativeMain)
-        }
-        linuxX64().compilations["main"].defaultSourceSet.dependsOn(linuxMain)
-        linuxArm64().compilations["main"].defaultSourceSet.dependsOn(linuxMain)
-
-        val linuxTest by creating {
-            dependsOn(nativeTest)
-        }
-        linuxX64().compilations["test"].defaultSourceSet.dependsOn(linuxTest)
-        linuxArm64().compilations["test"].defaultSourceSet.dependsOn(linuxTest)
-
-        // Windows
-        val mingwMain by creating {
-            dependsOn(nativeMain)
-        }
-        mingwX64().compilations["main"].defaultSourceSet.dependsOn(mingwMain)
-
-        val mingwTest by creating {
-            dependsOn(nativeTest)
-        }
-        mingwX64().compilations["test"].defaultSourceSet.dependsOn(mingwTest)
-
-        // Android Native
-        val androidNativeMain by creating {
-            dependsOn(nativeMain)
-        }
-        androidNativeArm32().compilations["main"].defaultSourceSet.dependsOn(androidNativeMain)
-        androidNativeArm64().compilations["main"].defaultSourceSet.dependsOn(androidNativeMain)
-        androidNativeX86().compilations["main"].defaultSourceSet.dependsOn(androidNativeMain)
-        androidNativeX64().compilations["main"].defaultSourceSet.dependsOn(androidNativeMain)
-
-        val androidNativeTest by creating {
-            dependsOn(nativeTest)
-        }
-        androidNativeArm32().compilations["test"].defaultSourceSet.dependsOn(androidNativeTest)
-        androidNativeArm64().compilations["test"].defaultSourceSet.dependsOn(androidNativeTest)
-        androidNativeX86().compilations["test"].defaultSourceSet.dependsOn(androidNativeTest)
-        androidNativeX64().compilations["test"].defaultSourceSet.dependsOn(androidNativeTest)
     }
 }
 
